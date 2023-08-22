574aa1d2f0640ace64180bb06a84ab0c5a56cb3e
GeoSauer <geoffrey.sauer89@gmail.com>
2023-08-22 11:06:50 -0600

    initial setup

---
+++ src/app.ts
@@ -0,0 +1,6 @@
+// const express = require("express"); //this is the actual Node import syntax
+import express from "express"; //after installing the Node/Express Types, module imports work with TS support
+
+const app = express();
+
+app.listen(3000);
