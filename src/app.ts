35cb5c553a562e303fcbd0deaf685da6a81f2a51
GeoSauer <geoffrey.sauer89@gmail.com>
2023-08-22 11:15:41 -0600

    adds basic routes and error handling middleware

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,15 @@
 // const express = require("express"); //this is the actual Node import syntax
-import express from "express"; //after installing the Node/Express Types, module imports work with TS support
+import express, { Request, Response, NextFunction } from "express"; //after installing the Node/Express Types, module imports work with TS support
+
+import todoRoutes from "./routes/todos";
 
 const app = express();
+
+app.use("/todos", todoRoutes);
+
+// app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {}); //alternate to importing
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({ message: err.message });
+});
 
 app.listen(3000);
